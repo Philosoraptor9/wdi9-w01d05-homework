de730a05e4930e220a258afae8affb82109c4b87
Philip Hoover <philhoov90@gmail.com>
2018-09-16 22:09:55 -0600

    Variables and Data Types - Q & A

---
+++ js/hw.js
@@ -0,0 +1,13 @@
+console.log("and so it begins");
+
+// I. VARIABLES AND DATA TYPES
+
+// A. Q & A
+// 1. We use the '=' sign to assign a value to a variable
+// 2. To change the value of a variable, simply re-state the variable and assign it the new value
+// 3. To assign an existing variable to a new variable, assign the new variable's value to be the existing variable
+// 4. Declaring a variable brings it into existence; assigning a value means using the equals sign to give the variable a value;
+// definitions of a variable can change, i.e. a variable can be re-defined by assigning it a new value
+// 5. Pseudocoding is just writing out the steps of the process you are trying to code in plain english. This helps
+// organize your thoughts before trying to translate them into code.
+// 6. 75-95% planning, 10-25% coding
